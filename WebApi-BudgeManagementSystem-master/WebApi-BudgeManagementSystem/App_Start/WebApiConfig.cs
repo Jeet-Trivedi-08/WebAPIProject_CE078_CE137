804bde48532643cc10e88e7e678363a00fd73c73
Jeet-Trivedi-08 <68767054+Jeet-Trivedi-08@users.noreply.github.com>
2021-04-03 02:21:57 +0530

    Finnal Commit

---
+++ WebApi-BudgeManagementSystem-master/WebApi-BudgeManagementSystem/App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace WebApi_BudgeManagementSystem
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            //{action} is edited by chetan
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
